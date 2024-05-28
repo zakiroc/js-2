72acac287aca4afc4c6a043a850d5f3d6203fa58
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-05-28 15:47:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+alert('sizda hozir random sonlar chiqishni boshlaydi')
+
+let randomnum = Math.floor(Math.random() *100)
+alert(`random orqali chiqgan son : ${randomnum}`)
+confirm('biz random sonni 2 ga kopaytirmoqchi va bolmoqchimiz !')
+
+alert(`
+random son chiqgan: ${randomnum}
+uni biza 2 ga kopaytiramiz javob:${randomnum*2}
+va endi biz 2 ga bolamiz javob:${randomnum/2}
+va shu sonni 3 ga bolgandagi qoldiq:${randomnum%3}
+
+
+
+`)
+
